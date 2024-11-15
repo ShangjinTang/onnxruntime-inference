dd2d704c1b1e355e77ac3a8365e73339b7074533
Shangjin Tang <shangjin.tang@gmail.com>
2024-11-15 16:07:54 +0800

    feat: refactor to class to make code resuable

--- onnx_inference_cpp_dynamic/main.cc
+++ onnx_inference_cpp_dynamic/main.cc
@@ -8,121 +8,145 @@
 #include "onnxruntime_cxx_api.h"
 #undef ORT_API_MANUAL_INIT
 
+using GetApiBaseFunc = OrtApiBase* (*)();
+
+class OnnxClassifier {
+public:
+    explicit OnnxClassifier(const std::string& model_path)
+            : env_(ORT_LOGGING_LEVEL_WARNING, "ONNX_Cpp_API"),
+              session_(env_, model_path.c_str(), Ort::SessionOptions{}) {
+        session_options_.DisableCpuMemArena();
+    }
+
+    void setInput2D(const std::vector<std::vector<float>>& input_data_2d) {
+        // Flatten the 2D array into a 1D vector
+        input_data_.clear();
+        for (const auto& row : input_data_2d) {
+            input_data_.insert(input_data_.end(), row.begin(), row.end());
+        }
+
+        // Create a vector to hold the input tensor with shape [n, m]
+        n_ = input_data_2d.size();
+        m_ = input_data_2d[0].size();
+        input_shape_ = {n_, m_};
+
+        // Create the input tensor
+        auto memory_info = Ort::MemoryInfo::CreateCpu(OrtArenaAllocator, OrtMemTypeDefault);
+        input_tensor_ = Ort::Value::CreateTensor<float>(memory_info, input_data_.data(), input_data_.size(),
+                                                       input_shape_.data(), input_shape_.size());
+    }
+
+    void runInference() {
+        std::array<const char*, 1> input_names = {"input"};
+        std::array<const char*, 2> output_names = {"output_label", "output_probability"};
+
+        // Perform inference
+        output_tensors_ = session_.Run(Ort::RunOptions{nullptr}, input_names.data(), &input_tensor_, input_names.size(),
+                                     output_names.data(), output_names.size());
+    }
+
+    void printResults() const {
+        // Process the output
+        auto& output_tensor = const_cast<Ort::Value &>(output_tensors_.front());
+        // Caution: be careful with the output tensor type, otherwise might get error values.
+        auto* output_data = output_tensor.GetTensorMutableData<int64_t>();
+
+        for (size_t i = 0; i < output_tensor.GetTensorTypeAndShapeInfo().GetElementCount(); ++i) {
+            std::cout << "Input: ";
+            for (size_t j = 0; j < m_; ++j) {  // Assuming 4 features in each input sample
+                std::cout << std::fixed << std::setprecision(1) << input_data_[(i * m_) + j]; // Adjust indexing
+                if (j != m_) {
+                    std::cout << ", ";
+                }
+            }
+            std::cout << " | Output Label: " << output_data[i] << '\n';
+        }
+    }
+
+private:
+    Ort::Env env_;
+    Ort::Session session_;
+    Ort::SessionOptions session_options_;
+
+    std::vector<float> input_data_;
+    std::vector<int64_t> input_shape_;
+    Ort::Value input_tensor_{nullptr};
+
+    std::vector<Ort::Value> output_tensors_;
+    int n_{}; // Number of samples
+    int m_{}; // Number of dimensions
+};
+
+
 int main() {
     const char* model_path = "./xgbc_iris.onnx";
 
     // NOTE: It's better to use absolute link in dlopen. Relative link below is only for demostration.
     void* ort_library = dlopen("./onnxruntime/lib/libonnxruntime.so.1", RTLD_LAZY);
-    if (!ort_library) {
-        std::cerr << "Failed to load libonnxruntime.so: " << dlerror() << std::endl;
+    if (ort_library == nullptr) {
+        std::cerr << "Failed to load libonnxruntime.so: " << dlerror() << '\n';
         return 1;
     }
 
-    OrtApiBase* (*GetApiBase)() = reinterpret_cast<OrtApiBase* (*)()>(dlsym(ort_library, "OrtGetApiBase"));
-    if (!GetApiBase) {
+    auto get_api_base = reinterpret_cast<GetApiBaseFunc>(dlsym(ort_library, "OrtGetApiBase"));
+    if (get_api_base == nullptr) {
         dlclose(ort_library);
         return 1;
     }
 
-    const OrtApi* api = GetApiBase()->GetApi(ORT_API_VERSION);
+    const OrtApi* api = get_api_base()->GetApi(ORT_API_VERSION);
 
     Ort::InitApi(api);
 
-    if (!Ort::Global<void>::api_) {
-        std::cerr << "Failed to int api" << std::endl;
-        return 1;
-    }
-
     {
-        Ort::Env env(ORT_LOGGING_LEVEL_WARNING, "ONNX_Cpp_API");
-        Ort::SessionOptions session_options;
-        session_options.DisableCpuMemArena();
-        Ort::Session session(env, model_path, session_options);
+        // Create an instance of OnnxClassifier
+        OnnxClassifier classifier(model_path);
 
-        // Set the input and output names
-        // Currently we use `onnx (python lib)` or `netron (online tool)` to get the node names.
-        // TODO: check if can be read using C/C++ API without external tools.
-        std::vector<const char*> input_node_names = {"input"};
-        std::vector<const char*> output_node_names = {"output_label", "output_probability"};
-
-        // Define a 2D array for test samples
-        // clang-format off
-        std::vector<std::vector<float>> input_data_2d = {
-            {6.1, 2.8, 4.7, 1.2},
-            {5.7, 3.8, 1.7, 0.3},
-            {7.7, 2.6, 6.9, 2.3},
-            {6.0, 2.9, 4.5, 1.5},
-            {6.8, 2.8, 4.8, 1.4},
-            {5.4, 3.4, 1.5, 0.4},
-            {5.6, 2.9, 3.6, 1.3},
-            {6.9, 3.1, 5.1, 2.3},
-            {6.2, 2.2, 4.5, 1.5},
-            {5.8, 2.7, 3.9, 1.2},
-            {6.5, 3.2, 5.1, 2.0},
-            {4.8, 3.0, 1.4, 0.1},
-            {5.5, 3.5, 1.3, 0.2},
-            {4.9, 3.1, 1.5, 0.1},
-            {5.1, 3.8, 1.5, 0.3},
-            {6.3, 3.3, 4.7, 1.6},
-            {6.5, 3.0, 5.8, 2.2},
-            {5.6, 2.5, 3.9, 1.1},
-            {5.7, 2.8, 4.5, 1.3},
-            {6.4, 2.8, 5.6, 2.2},
-            {4.7, 3.2, 1.6, 0.2},
-            {6.1, 3.0, 4.9, 1.8},
-            {5.0, 3.4, 1.6, 0.4},
-            {6.4, 2.8, 5.6, 2.1},
-            {7.9, 3.8, 6.4, 2.0},
-            {6.7, 3.0, 5.2, 2.3},
-            {6.7, 2.5, 5.8, 1.8},
-            {6.8, 3.2, 5.9, 2.3},
-            {4.8, 3.0, 1.4, 0.3},
-            {4.8, 3.1, 1.6, 0.2}
+        // Define test samples (input data)
+        const std::vector<std::vector<float>> input_data_2d = {
+                {6.1, 2.8, 4.7, 1.2},
+                {5.7, 3.8, 1.7, 0.3},
+                {7.7, 2.6, 6.9, 2.3},
+                {6.0, 2.9, 4.5, 1.5},
+                {6.8, 2.8, 4.8, 1.4},
+                {5.4, 3.4, 1.5, 0.4},
+                {5.6, 2.9, 3.6, 1.3},
+                {6.9, 3.1, 5.1, 2.3},
+                {6.2, 2.2, 4.5, 1.5},
+                {5.8, 2.7, 3.9, 1.2},
+                {6.5, 3.2, 5.1, 2.0},
+                {4.8, 3.0, 1.4, 0.1},
+                {5.5, 3.5, 1.3, 0.2},
+                {4.9, 3.1, 1.5, 0.1},
+                {5.1, 3.8, 1.5, 0.3},
+                {6.3, 3.3, 4.7, 1.6},
+                {6.5, 3.0, 5.8, 2.2},
+                {5.6, 2.5, 3.9, 1.1},
+                {5.7, 2.8, 4.5, 1.3},
+                {6.4, 2.8, 5.6, 2.2},
+                {4.7, 3.2, 1.6, 0.2},
+                {6.1, 3.0, 4.9, 1.8},
+                {5.0, 3.4, 1.6, 0.4},
+                {6.4, 2.8, 5.6, 2.1},
+                {7.9, 3.8, 6.4, 2.0},
+                {6.7, 3.0, 5.2, 2.3},
+                {6.7, 2.5, 5.8, 1.8},
+                {6.8, 3.2, 5.9, 2.3},
+                {4.8, 3.0, 1.4, 0.3},
+                {4.8, 3.1, 1.6, 0.2}
         };
-        // clang-format on
 
-        // Flatten the 2D array into a 1D vector
-        std::vector<float> input_data;
-        for (const auto& row : input_data_2d) {
-            input_data.insert(input_data.end(), row.begin(), row.end());
-        }
+        // Set input data and run inference
+        classifier.setInput2D(input_data_2d);
+        classifier.runInference();
 
-        // Create a vector to hold the input tensor with shape [n, 4]
-        int n = input_data_2d.size();
-
-        // Create a vector to hold the input tensor with shape [n, 4]
-        std::vector<int64_t> input_shape = {n, 4};
-
-        auto memory_info = Ort::MemoryInfo::CreateCpu(OrtArenaAllocator, OrtMemTypeDefault);
-        Ort::Value input_tensor = Ort::Value::CreateTensor<float>(memory_info, input_data.data(), input_data.size(),
-                                                                  input_shape.data(), input_shape.size());
-
-        // Perform inference
-        std::vector<Ort::Value> output_tensors =
-                session.Run(Ort::RunOptions{nullptr}, input_node_names.data(), &input_tensor, 1,
-                            output_node_names.data(), output_node_names.size());
-
-        // Process the output
-        Ort::Value& output_tensor = output_tensors.front();
-        // Caution: be careful with the output tensor type, otherwise might get error values.
-        int64_t* output_data = output_tensor.GetTensorMutableData<int64_t>();
-
-        // Print the output
-        for (size_t i = 0; i < output_tensor.GetTensorTypeAndShapeInfo().GetElementCount(); ++i) {
-            std::cout << "Input: ";
-            for (size_t j = 0; j < input_data_2d[i].size(); ++j) {
-                std::cout << std::fixed << std::setprecision(1) << input_data_2d[i][j];
-                if (j < input_data_2d.size() - 1) {
-                    std::cout << ", ";
-                }
-            }
-            std::cout << " | Output: " << output_data[i] << std::endl;
-        }
+        // Print the results
+        classifier.printResults();
     }
 
-    int result = dlclose(ort_library);
+    const int result = dlclose(ort_library);
     if (result != 0) {
-        std::cerr << "Failed to call dlclose()" << std::endl;
+        std::cerr << "Failed to call dlclose()" << '\n';
         return 1;
     }
 
